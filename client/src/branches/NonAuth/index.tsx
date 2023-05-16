10c061fa6ceb96743a3444d10b038aa0873f3c69
JamesDeChavez <jamesdechavez23@gmail.com>
2023-05-16 15:33:53 -0700

    removed lazy load of sample

--- client/src/branches/NonAuth/index.tsx
+++ client/src/branches/NonAuth/index.tsx
@@ -1,9 +1,6 @@
-import React, { Suspense, lazy, useState } from "react"
+import React, { useState } from "react"
 import LandingPage from "../../pages/Landing"
-// import SamplePage from "../../pages/Sample"
-import SuspenseLoad from "../../components/SuspenseLoad"
-
-const SamplePage = lazy(() => import("../../pages/Sample"))
+import SamplePage from "../../pages/Sample"
 
 export const NonAuthRenderContext = React.createContext<{
     RENDERS: string[],
@@ -22,9 +19,7 @@ const NonAuthBranch = () => {
     return (
     <NonAuthRenderContext.Provider value={{RENDERS, render, setRender}}>
         {render === RENDERS[3] ?
-            <Suspense fallback={<SuspenseLoad/>} >
-                <SamplePage/> 
-            </Suspense> 
+            <SamplePage/> 
         :
             <LandingPage/>
         }
