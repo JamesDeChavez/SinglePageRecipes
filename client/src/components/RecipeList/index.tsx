37fb5a50518330a20ca154ac537006590b30995a
JamesDeChavez <jamesdechavez23@gmail.com>
2023-07-26 10:18:22 -0700

    added filter functionality to recipe book page

--- client/src/components/RecipeList/index.tsx
+++ client/src/components/RecipeList/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useContext, useRef, useLayoutEffect } from 'react'
+import { useState, useContext, useRef, useEffect, useLayoutEffect } from 'react'
 import { client } from '../../index'
 import { UserLoggedInContext } from '../../App'
 import { RecipesFragment } from '../../graphql/fragments'
@@ -11,8 +11,48 @@ import './styles.css'
 const RecipeList = () => {
     const { userId } = useContext(UserLoggedInContext)
     const { recipes } = client.readFragment({ id: `User:${userId}`, fragment: RecipesFragment }) ?? { recipes: [] }
+    const [recipesToRender, setRecipesToRender] = useState<Recipe[]>(recipes)
     const [search, setSearch] = useState('')
+    const [proteinFilter, setProteinFilter] = useState('')
+    const [vegetableFilter, setVegetableFilter] = useState('')
+    const [carbFilter, setCarbFilter] = useState('')
     const root = useRef(null)
+
+    useEffect(() => {
+        let newRender = recipes.filter((recipe: any) => recipe.title.toLowerCase().includes(search.toLowerCase()))
+
+        if (proteinFilter !== '') {
+            newRender = newRender.filter((recipe: any) => {
+                const ingredients = recipe.ingredients
+                for (let i = 0; i < ingredients.length; i++) {
+                    if (ingredients[i].name.toLowerCase().includes(proteinFilter.toLowerCase())) return true
+                }
+                return false
+            })        
+        }
+
+        if (vegetableFilter !== '') {
+            newRender = newRender.filter((recipe: any) => {
+                const ingredients = recipe.ingredients
+                for (let i = 0; i < ingredients.length; i++) {
+                    if (ingredients[i].name.toLowerCase().includes(vegetableFilter.toLowerCase())) return true
+                }
+                return false
+            })        
+        }
+
+        if (carbFilter !== '') {
+            newRender = newRender.filter((recipe: any) => {
+                const ingredients = recipe.ingredients
+                for (let i = 0; i < ingredients.length; i++) {
+                    if (ingredients[i].name.toLowerCase().includes(carbFilter.toLowerCase())) return true
+                }
+                return false
+            })        
+        }
+
+        setRecipesToRender(newRender)
+    }, [search, proteinFilter, vegetableFilter, carbFilter, recipes])
 
     useLayoutEffect(() => {
         const gsapContext = gsap.context(() => {
@@ -34,22 +74,22 @@ const RecipeList = () => {
             <div className={`${className}_filtersSidebar`}>
                 <div className={`${className}_filterContainer`}>
                     <label htmlFor="protein" className={`${className}_filterText`}>Protein Filter</label>
-                    <select name="protein" id="protein" className={`${className}_proteinSelect`}>
-                        <option className={`${className}_option`} value="null"></option>
+                    <select name="protein" id="protein" className={`${className}_proteinSelect`} onChange={(e) => setProteinFilter(e.target.value)}>
+                        <option className={`${className}_option`} value=""></option>
                         <option className={`${className}_option`} value="Chicken">Chicken</option>
                     </select>
                 </div>
                 <div className={`${className}_filterContainer`}>
                     <label htmlFor="vegetable" className={`${className}_filterText`}>Vegetable Filter</label>
-                    <select name="vegetable" id="vegetable" className={`${className}_vegetableSelect`}>
-                        <option className={`${className}_option`} value="null"></option>
+                    <select name="vegetable" id="vegetable" className={`${className}_vegetableSelect`} onChange={(e) => setVegetableFilter(e.target.value)}>
+                        <option className={`${className}_option`} value=""></option>
                         <option className={`${className}_option`} value="Broccoli">Broccoli</option>
                     </select>
                 </div>
                 <div className={`${className}_filterContainer`}>
                     <label htmlFor="carb" className={`${className}_filterText`}>Carb Filter</label>
-                    <select name="carb" id="carb" className={`${className}_carbSelect`}>
-                        <option className={`${className}_option`} value="null"></option>
+                    <select name="carb" id="carb" className={`${className}_carbSelect`} onChange={(e) => setCarbFilter(e.target.value)}>
+                        <option className={`${className}_option`} value=""></option>
                         <option className={`${className}_option`} value="Pasta">Pasta</option>
                     </select>
                 </div>
@@ -57,7 +97,7 @@ const RecipeList = () => {
             </div>
             <div className={`${className}_recipesContainer`}>
                 {recipes.length ?
-                    recipes.filter((recipe: any) => recipe.title.toLowerCase().includes(search.toLowerCase())).map((recipe: Recipe, i: number) => {
+                    recipesToRender.map((recipe: Recipe, i: number) => {
                         return <RecipeOption key={i} recipe={recipe} />
                     })                
                 : <p className={`${className}_text`}>No Recipes Created</p> }
